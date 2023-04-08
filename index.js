097e79cb2863d61ad7e7fbe2d6906a0f5d39383e
Brittany Boughter <brittany.wood0308@yahoo.com>
2023-04-07 19:00:19 -0700

    added required classes to the index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const SVG = require('./lib/svg');
+const Circle = require('./lib/circle.js');
+const Square = require('./lib/square.js');
+const Triangle = require('./lib/triangle.js')
+
