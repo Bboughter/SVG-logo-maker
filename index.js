33393a85229d3ab04cae851ca7a37c59d445c4af
Brittany Boughter <brittany.wood0308@yahoo.com>
2023-04-07 19:10:01 -0700

    added svg.js

--- index.js
+++ index.js
@@ -1,7 +1,31 @@
-const inquirer = require('inquirer');
-const fs = require('fs');
-const SVG = require('./lib/svg');
-const Circle = require('./lib/circle.js');
-const Square = require('./lib/square.js');
-const Triangle = require('./lib/triangle.js')
+// const inquirer = require('inquirer');
+// const fs = require('fs');
+// const SVG = require('./lib/svg');
+// const Circle = require('./lib/circle.js');
+// const Square = require('./lib/square.js');
+// const Triangle = require('./lib/triangle.js')
 
+// inquirer
+// .prompt ([
+//     {
+//         type: 'input',
+//         name: 'characters',
+//         message: 'Enter three characters for your logo',
+//     },
+//     {
+//         type: 'input',
+//         name: 'textColor',
+//         message: 'What color would you like the text to be?',
+//     },
+//     {
+//         type: 'list',
+//         name: 'shape',
+//         message: 'Choose a shape:',
+//         choices: ['circle', ['square'],]
+//     },
+//     {
+//         type: 'input',
+//         name: 'characters',
+//         message: 'Enter three characters for your logo',
+//     }
+// ])
